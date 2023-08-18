8ad41d180d879711cab8c1bb0eb37d1b3f316c57
Cristhian <143498+gcca@users.noreply.github.com>
2023-08-18 17:33:46 -0500

    misc (#171)

--- riesgosgenerales/settings.py
+++ riesgosgenerales/settings.py
@@ -125,6 +125,12 @@ USE_TZ = True
 
 STATIC_URL = "static/"
 
+if not DEBUG:
+    STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+    STATICFILES_STORAGE = (
+        "whitenoise.storage.CompressedManifestStaticFilesStorage"
+    )
+
 # Media files (user uploaded files)
 
 MEDIA_URL = "/media/"
