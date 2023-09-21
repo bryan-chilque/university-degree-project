c9d9019b385dcf5fbcf5aaa01483b65a02a8b4db
Paulo Llanos <37987149+paulo1403@users.noreply.github.com>
2023-09-20 22:09:45 -0500

    feat: add imports

--- riesgosgenerales/urls.py
+++ riesgosgenerales/urls.py
@@ -1,7 +1,8 @@
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import include, path
+from django.contrib.staticfiles.views import serve
+from django.urls import include, path, re_path
 from django.views.generic import RedirectView
 
 urlpatterns = [
