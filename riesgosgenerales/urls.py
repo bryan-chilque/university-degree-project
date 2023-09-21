d13e1d2f4f31a1e16bfbd635c2a4d02071c89d70
Paulo Llanos <37987149+paulo1403@users.noreply.github.com>
2023-09-20 21:57:06 -0500

    feat: improve media render

--- riesgosgenerales/urls.py
+++ riesgosgenerales/urls.py
@@ -1,8 +1,9 @@
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import include, path
+from django.urls import include, path, re_path
 from django.views.generic import RedirectView
+from django.views.static import serve
 
 urlpatterns = [
     path("", RedirectView.as_view(pattern_name="rrggweb:login")),
@@ -11,3 +12,10 @@ urlpatterns = [
     path("admin/", include("rrggadmin.urls")),
     path("_/", admin.site.urls),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+if settings.DEBUG:
+    urlpatterns += [
+        re_path(r'^media/(?P<path>.*)$', serve, {
+            'document_root': settings.MEDIA_ROOT,
+        }),
+    ]
